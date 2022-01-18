111865f55f499cd8be62d1cb504d635e549fc0ff
doni002 <alpardonat02@gmail.com>
2022-01-18 20:54:01 +0100

    Add js to menu

---
+++ menu.js
@@ -0,0 +1,9 @@
+
+function rotate(){
+    let button = document.getElementById("menuButton");
+    let miniDisc = document.getElementById("miniDisc");
+    button.classList.add("rotating");
+    miniDisc.classList.add("hideDisc");
+}
+
+
