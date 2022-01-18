3ce82f13975f5c01001eb916b20def4dee4174b8
benedekhalaj <bendihalaj@gmail.com>
2022-01-18 18:44:38 +0100

    Add audio for testing purposes

--- presentation.js
+++ presentation.js
@@ -1,12 +1,14 @@
-const gra = function(min, max) {
-    return Math.random() * (max - min) + min;
-}
-const init = function(){
-  let items = document.querySelectorAll('section');
-  for (let i = 0; i < items.length; i++){
-    items[i].style.background = randomColor({luminosity: 'light'});
-  }
-  cssScrollSnapPolyfill()
-}
-init();
+const playBtn = document.querySelector('.play-btn');
+const stopBtn = document.querySelector('.stop-btn');
+const compactDisk = document.querySelector('.fa-compact-disc');
+const audio = document.getElementById("myAudio");
 
+playBtn.addEventListener('click', function() {
+    compactDisk.classList.add('rotate');
+    audio.play();
+})
+
+stopBtn.addEventListener('click', function() {
+    compactDisk.classList.remove('rotate');
+    audio.pause();
+})
