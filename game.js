d0c0671ef0c478aeb84b9018a96613977a1b56ed
Nataniel Latorre <latorre.nataniel@hotmail.com>
2022-01-18 11:11:29 +0100

    Create game area

--- game.js
+++ game.js
@@ -1,7 +1,56 @@
-initGame();
+import * as movement from "./recursivemovement.js"
 
-function initGame() {
+const GAME_AREA_HEIGHT = 20;
+const GAME_AREA_WIDTH = 30;
 
-    // Your game can start here, but define separate functions, don't write everything in here :)
 
+function createElement(classOfElement) {
+    if (classOfElement === "player") {
+        let newDiv = document.createElement("div");
+        newDiv.className = "tile player fas fa-angry";
+        return newDiv
+    } else {
+    let newDiv = document.createElement("div");
+    newDiv.className = "tile fas";
+    classOfElement ? newDiv.classList.add(classOfElement) : {/*pass*/};
+    return newDiv
+    }
 }
+
+function addRow() {
+    const display = document.getElementById("display");
+    const newRow = document.createElement("div");
+    newRow.className = "row";
+    display.appendChild(newRow);
+    return newRow
+}
+
+function createTable() {
+    for (let i = 0; i < GAME_AREA_HEIGHT; i++) {
+        let newRow = addRow();
+        for (let j = 0; j < GAME_AREA_WIDTH; j++) {
+            if (i === 0 && j === 0) {
+                let newTile = createElement("player");
+                newTile.dataset.row = i;
+                newTile.dataset.column = j;
+                newRow.appendChild(newTile);
+            } else {
+                let newTile = createElement();
+                newTile.dataset.row = i;
+                newTile.dataset.column = j;
+                newRow.appendChild(newTile);
+            }
+        }
+    }
+}
+
+function initMovement() {
+    document.addEventListener('keydown', function(event) {
+        movement.go(event.key)
+    });
+}
+
+
+
+createTable();
+initMovement();
