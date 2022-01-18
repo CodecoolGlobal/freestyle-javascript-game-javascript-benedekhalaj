f17dc8dd5767c10d6565ecaf3d59858a5898dd37
benedekhalaj <bendihalaj@gmail.com>
2022-01-18 20:59:49 +0100

    Add setTimeout to rotate function for testing purposes

--- menu.js
+++ menu.js
@@ -1,9 +1,11 @@
-
 function rotate(){
     let button = document.getElementById("menuButton");
     let miniDisc = document.getElementById("miniDisc");
     button.classList.add("rotating");
     miniDisc.classList.add("hideDisc");
+    setTimeout(function() {
+        console.log('animation ended')
+    }, 2300)
 }
 
 
