ec27bb563be758350e12b4e5bcdb446c0d1454f1
Nataniel Latorre <latorre.nataniel@hotmail.com>
2022-01-19 17:15:01 +0100

    Fix class addition on reset position button

--- reset_position.js
+++ reset_position.js
@@ -1,10 +1,9 @@
 const AXIS_X_START = 0;
 const AXIS_Y_START = 0;
-const playerIcon = "fa-angry"
 
 function resetPosition(){
     let player = document.querySelector(".player");
-    player.classList.remove("player", `${playerIcon}`);
+    player.classList.remove("player");
     let startingPoint = document.querySelector(`[data-row="${AXIS_Y_START}"][data-column="${AXIS_X_START}"]`);
-    startingPoint.classList.add("player", `${playerIcon}`);
+    startingPoint.classList.add("player");
 }
