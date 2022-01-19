4ba11dd4b31f06cdb6b54c0d36c20aabb9b1a48f
Nataniel Latorre <latorre.nataniel@hotmail.com>
2022-01-19 17:11:23 +0100

    Add goal area to win level

--- movement.js
+++ movement.js
@@ -12,6 +12,9 @@ function setDirection(direction) {
     return arrowDirection[direction]
 }
 
+function checkWin(player) {
+    return player.classList.contains("goal");
+}
 
 export function go(direction) {
     let [directionRow, directionCol] = setDirection(direction);
@@ -19,8 +22,8 @@ export function go(direction) {
     let playerColumn = Number(player.dataset.column);
     let playerRow = Number(player.dataset.row);
     let neighbour = document.querySelector(`[data-row="${playerRow + directionRow}"][data-column="${playerColumn + directionCol}"]`);
-    if (neighbour.classList.contains("goal")){
-        console.log("you won")
+    if (checkWin(player)) {
+        console.log("you've won");
     } else if (neighbour === null) {
         player.classList.remove("player");
         let startingPoint = document.querySelector(`[data-row="${AXIS_Y_START}"][data-column="${AXIS_X_START}"]`);
