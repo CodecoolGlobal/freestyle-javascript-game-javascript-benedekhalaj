33250bb4622cec056cba0790937510d3c0ffb347
Nataniel Latorre <latorre.nataniel@hotmail.com>
2022-01-20 20:04:13 +0100

    Create object to store step sounds

---
+++ soundfx.js
@@ -0,0 +1,16 @@
+function playStepSound(pitch) {
+    const stepSoundBank = {
+        "1": new Audio(),
+        "2": new Audio(),
+        "3": new Audio(),
+        "4": new Audio(),
+        "5": new Audio(),
+        "6": new Audio(),
+        "7": new Audio(),
+        "8": new Audio(),
+        "9": new Audio(),
+        "10": new Audio(),
+        "11": 
+    }
+    return stepSoundBank[pitch]
+}
